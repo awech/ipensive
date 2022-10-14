0ce4ca085e28f9cafecaa2e0b249a98bec9a1cbf
awech <awech@usgs.gov>
2022-10-14 19:31:45 +0000

    NEIC-specific config file

---
+++ NEIC_config.py
@@ -0,0 +1,176 @@
+# SYSTEM PARAMETERS
+HOSTNAME	  = '137.227.224.220'
+PORT		  = 16002
+OUT_WEB_DIR   = '/www/avosouth.wr.usgs.gov/htdocs/infrasound'	# html & image ouptut directory
+OUT_ASCII_DIR = '/www/avosouth.wr.usgs.gov/htdocs/infrasound/ascii_output'	# ascii output directory (delete if undesired)
+LOGS_DIR	  = ''
+
+# DEFAUlT PROCESSING PARAMETERS
+DURATION      = 600  # DON'T CHANGE THIS!
+LATENCY       = 60	 # how long to pause and wait for data latency to catch up before processing (seconds)
+EXTRA_PAUSE   = 0 	 # extra pause in case some arrays are extra latent (Dillingham)
+FREQMIN		  = 0.8  # minimum frequency
+FREQMAX		  = 5.0  # maximum frequency
+TAPER	      = 5.0  # seconds to taper beginning and end of trace before filtering
+WINDOW_LENGTH = 30   # window length for each calculation [seconds]
+OVERLAP       = 15   # amount of overlap between windos [seconds]
+MIN_CHAN      = 3	 # minimum number of channels needed to perform inversion
+
+# DEFAULT PLOTTING PARAMETERS
+MCTHRESH      = 0.6  # where to draw the threshold line in MCCM plot
+AZ_MIN		  = 0	 # minimum azimuth to plot
+AZ_MAX		  = 360	 # maximum azimuth to plot
+VEL_MIN		  = 0.25 # minimum sound speed range
+VEL_MAX		  = 0.45 # maximum sound speed range
+ARRAY_LABEL	  = 'Infrasound'
+
+# Infrasound channels list
+NETWORKS=[
+	{
+		'Name' : 'AVO',
+		'ARRAYS' : [
+
+			dict({'Name':'Kenai',
+				  'SCNL':[
+							{'scnl':'KENI.HDF.AV.01'	, 'sta_lat': 60.6413700	, 'sta_lon': -151.070200},
+							{'scnl':'KENI.HDF.AV.02'	, 'sta_lat': 60.6404567 , 'sta_lon': -151.070330},
+							{'scnl':'KENI.HDF.AV.03'	, 'sta_lat': 60.6406033	, 'sta_lon': -151.072020},
+							{'scnl':'KENI.HDF.AV.04'	, 'sta_lat': 60.6412000	, 'sta_lon': -151.073000},
+							{'scnl':'KENI.HDF.AV.05'	, 'sta_lat': 60.6415300	, 'sta_lon': -151.072000},
+							{'scnl':'KENI.HDF.AV.06'	, 'sta_lat': 60.6409167 , 'sta_lon': -151.071170},
+						],
+				'digouti': (1/419430.0)/(0.0275),
+				'volcano':[
+							{'name': 'Wrangell',  'v_lat': 62.00572,  'v_lon': -144.01935},
+							{'name': 'Spurr',     'v_lat': 61.29897,  'v_lon': -152.25122},
+							{'name': 'Redoubt',   'v_lat': 60.48576,  'v_lon': -152.74282},
+							{'name': 'Iliamna',   'v_lat': 60.03220,  'v_lon': -153.09002},
+							{'name': 'Augustine', 'v_lat': 59.36107,  'v_lon': -153.42938},
+						  ],
+				'AZ_MIN': 200,
+				'AZ_MAX': 80
+			}),
+
+			dict({'Name':'Sand Point',
+				  'SCNL':[
+							{'scnl':'SDPI.HDF.AV.01'	, 'sta_lat': 55.34900	, 'sta_lon': -160.47640},
+							{'scnl':'SDPI.HDF.AV.02'	, 'sta_lat': 55.34870	, 'sta_lon': -160.47683},
+							{'scnl':'SDPI.HDF.AV.03'	, 'sta_lat': 55.34934	, 'sta_lon': -160.47732},
+							{'scnl':'SDPI.HDF.AV.04'	, 'sta_lat': 55.34952	, 'sta_lon': -160.47661},
+							{'scnl':'SDPI.HDF.AV.05'	, 'sta_lat': 55.34922	, 'sta_lon': -160.47650},
+							{'scnl':'SDPI.HDF.AV.06'	, 'sta_lat': 55.34919	, 'sta_lon': -160.47710},
+						 ],
+				'digouti': (1/419430.0)/0.0275,
+				'volcano':[
+			   				{'name': 'Pavlof',    'v_lat': 55.417622,'v_lon': -161.893669},
+			   				{'name': 'Veniaminof','v_lat': 56.195825,'v_lon': -159.389536},
+			   				{'name': 'Shishaldin','v_lat': 54.755856,'v_lon': -163.969961}
+		   				  ],
+			}),
+
+			dict({'Name':'Akutan',
+		  		  'SCNL':[
+							{'scnl':'AKS.BDF.AV.01'	, 'sta_lat': 54.11050	, 'sta_lon': -165.69773},
+							{'scnl':'AKS.BDF.AV.02'	, 'sta_lat': 54.11028	, 'sta_lon': -165.69618},
+							{'scnl':'AKS.BDF.AV.03'	, 'sta_lat': 54.11105	, 'sta_lon': -165.69700},
+							{'scnl':'AKS.BDF.AV.04'	, 'sta_lat': 54.11053	, 'sta_lon': -165.69683},
+					     ],
+				'digouti': (1/419430.0)/0.05,
+				'volcano':[
+							{'name': 'Makushin', 'v_lat': 53.8900,   'v_lon': -166.9200},
+							{'name': 'Akutan',   'v_lat': 54.1300,   'v_lon': -165.9900},
+							{'name': 'Westdahl', 'v_lat': 54.5200,   'v_lon': -164.6500}
+						  ],
+			}),
+
+			dict({'Name':'Okmok',
+				  'SCNL':[
+							{'scnl':'OKIF.HDF.AV.01'	, 'sta_lat': 53.41083004	, 'sta_lon': -167.91426701},
+							{'scnl':'OKIF.HDF.AV.02'	, 'sta_lat': 53.41001901	, 'sta_lon': -167.91366301},
+							{'scnl':'OKIF.HDF.AV.03'	, 'sta_lat': 53.40998297	, 'sta_lon': -167.91499598},
+							{'scnl':'OKIF.HDF.AV.04'	, 'sta_lat': 53.41029796	, 'sta_lon': -167.91431696},
+							{'scnl':'OKIF.HDF.AV.05'	, 'sta_lat': 53.41038496	, 'sta_lon': -167.91331901},
+							{'scnl':'OKIF.HDF.AV.06'	, 'sta_lat': 53.41045604	, 'sta_lon': -167.91544802},
+						],
+				'digouti': 1/(419430*.03),	# convert counts to Pressure in Pa (Q330 + Chaparral 64Vx original)
+				'volcano':[
+							{'name': 'Bogoslof', 'v_lat': 53.9310,   'v_lon': -168.0360},
+							{'name': 'Cleveland','v_lat': 52.8222,   'v_lon': -169.9464},
+							{'name': 'Okmok',	 'v_lat': 53.428865, 'v_lon': -168.131632},
+							{'name': 'Makushin', 'v_lat': 53.8900,   'v_lon': -166.9200}
+						  ],
+			}),
+
+			dict({'Name':'Cleveland',
+				  'SCNL':[
+							{'scnl':'CLCO.BDF.AV.01'	, 'sta_lat': 52.7864125000	, 'sta_lon': -169.7229250000},
+							{'scnl':'CLCO.BDF.AV.02'	, 'sta_lat': 52.7871866667	, 'sta_lon': -169.7244333330},
+							{'scnl':'CLCO.BDF.AV.03'	, 'sta_lat': 52.7874600000	, 'sta_lon': -169.7210166667},
+							{'scnl':'CLCO.BDF.AV.04'	, 'sta_lat': 52.7861266667	, 'sta_lon': -169.7203966667},
+							{'scnl':'CLCO.BDF.AV.05'	, 'sta_lat': 52.7851866667	, 'sta_lon': -169.7250066667},
+							{'scnl':'CLCO.BDF.AV.06'	, 'sta_lat': 52.785861	, 'sta_lon': -169.723179},
+						 ],
+				'digouti': (1/419430.0)/(0.0275),
+				'volcano':[
+							{'name': 'Cleveland',   'v_lat': 52.8222,   'v_lon': -169.9464},
+							{'name': 'Bogoslof', 'v_lat': 53.9310,   'v_lon': -168.0360}
+						  ],
+			}),
+
+			dict({'Name':'Adak',
+				  'SCNL':[
+							{'scnl':'ADKI.HDF.AV.01'	, 'sta_lat': 51.86190727	, 'sta_lon': -176.6438598},
+							{'scnl':'ADKI.HDF.AV.02'	, 'sta_lat': 51.86324162	, 'sta_lon': -176.6435998},
+							{'scnl':'ADKI.HDF.AV.03'	, 'sta_lat': 51.86226962	, 'sta_lon': -176.6446503},
+							{'scnl':'ADKI.HDF.AV.04'	, 'sta_lat': 51.86246609	, 'sta_lon': -176.6457851},
+							{'scnl':'ADKI.HDF.AV.05'	, 'sta_lat': 51.86326916	, 'sta_lon': -176.6461231},
+							{'scnl':'ADKI.HDF.AV.06'	, 'sta_lat': 51.86157572	, 'sta_lon': -176.6469340}],
+				'digouti': (1/419430.0)/(0.05),
+				'volcano':[
+							{'name':	'Cleveland',   'v_lat': 52.8222,   'v_lon': -169.9464},
+							{'name':	'Great Sitkin','v_lat': 52.077282, 'v_lon': -176.131317},
+							{'name':	'Moffett',     'v_lat': 51.931876, 'v_lon': -176.740191},
+							{'name': 'Semisopochnoi',	 'v_lat': 51.947,	'v_lon': 179.623}
+						  ],
+			}),
+
+			dict({'Name':'Amchitka',
+				  'SCNL':[
+							{'scnl':'AMKA.HDF.AV.01'	, 'sta_lat': 51.379130	, 'sta_lon': 179.30130},
+							{'scnl':'AMKA.HDF.AV.02'	, 'sta_lat': 51.378489	, 'sta_lon': 179.30183},
+							{'scnl':'AMKA.HDF.AV.03'	, 'sta_lat': 51.378105	, 'sta_lon': 179.301225},
+							{'scnl':'AMKA.HDF.AV.04'	, 'sta_lat': 51.37831	, 'sta_lon': 179.30028},
+							{'scnl':'AMKA.HDF.AV.05'	, 'sta_lat': 51.379055	, 'sta_lon': 179.30026},
+							{'scnl':'AMKA.HDF.AV.06'	, 'sta_lat': 51.37871	, 'sta_lon': 179.30093},
+						],
+				'digouti': (1/400000)/0.0275,
+				'volcano':[
+			   			   {'name':	'Little Sitkin','v_lat': 51.95,	'v_lon': 178.543},
+						   {'name': 'Semisopochnoi','v_lat': 51.947,'v_lon': 179.623},
+						   {'name': 'Gareloi',	    'v_lat': 51.79,	'v_lon':-178.794},
+						 ],
+				'AZ_MIN': 270,
+				'AZ_MAX': 90,
+			}),
+
+			dict({'Name':'Dillingham',
+				  'SCNL':[
+							{'scnl':'DLL.HDF.AV.01'	, 'sta_lat': 59.13988781	, 'sta_lon': -158.6209290},
+							{'scnl':'DLL.HDF.AV.02'	, 'sta_lat': 59.13620003	, 'sta_lon': -158.6053376},
+							{'scnl':'DLL.HDF.AV.03'	, 'sta_lat': 59.12904044	, 'sta_lon': -158.6146964},
+							{'scnl':'DLL.HDF.AV.04'	, 'sta_lat': 59.13602776	, 'sta_lon': -158.6142354},
+							{'scnl':'DLL.HDF.AV.05'	, 'sta_lat': 59.13509488	, 'sta_lon': -158.6136803},
+							{'scnl':'DLL.HDF.AV.06'	, 'sta_lat': 59.13532733	, 'sta_lon': -158.6155527},
+						 ],
+				'digouti': 1/33505.5968,   # convert counts to Pressure in Pa 01-Jun-2021
+				'volcano':[
+							{'name': 'Bogoslof', 'v_lat': 53.9310,   'v_lon': -168.0360},
+							{'name': 'Veniaminof','v_lat': 56.195825,'v_lon': -159.389536},
+							{'name': 'Semisopochnoi',	 'v_lat': 51.947,	'v_lon': 179.623}
+						  ],
+				'EXTRA_PAUSE': 90,
+			}),			
+		]
+	},
+
+]
